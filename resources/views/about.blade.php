3d85b01f320616b55e585317b44220a06efa6da1
Kachill4 <friscillachikana@gmail.com>
2022-10-14 22:00:21 +0700

    lanjut revisi

---
+++ resources/views/about.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.master')
+@section('title', 'About')
+@section('content')
+<h1 class="cover-heading">Tentang Kami</h1>
+    <p class="lead">
+        Tak Kenal maka tak sayang, jadi kenalan dulu yuk
+    </p>
+
+    <p class="text-left">
+        12 Maret 2021
+    </p>
+
+<div class="col-md-6 px-0">
+    <div class="map_container">
+      <div class="map-responsive">
+        <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1923.9552582897068!2d107.59658074988722!3d-6.930847601973615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e8a030faab5b%3A0x26280f9163a06582!2sJl.%20Raya%20Kopo%20No.70%2C%20Panjunan%2C%20Kec.%20Astanaanyar%2C%20Kota%20Bandung%2C%20Jawa%20Barat%2040232%2C%20Indonesia!5e0!3m2!1sen!2sus!4v1665758825716!5m2!1sen!2sus"
+
+        width="600" height="300"
+        frameborder="0" style="border:0; width: 100%; height:100%" allowfullscreen></iframe>
+      </div>
+    </div>
+  </div>
+
+@endsection

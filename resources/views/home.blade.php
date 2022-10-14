bd71938ef2de0105b67beceb40a7c87f2dc4af40
Kachill4 <friscillachikana@gmail.com>
2022-10-14 23:18:02 +0700

    uhuhu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,11 @@
 <h1 class="cover-heading">M I S O&nbsp;&nbsp;&nbsp;K A M P U N G<br>
     B&nbsp;&nbsp;A&nbsp;&nbsp;H&nbsp;&nbsp;A&nbsp;&nbsp;G&nbsp;&nbsp;I&nbsp;&nbsp;A</h1>
 <p class="lead">
-MyCompany is a multi-disciplinary, independently-owned design studio.
-</p>
+Mini foodcourt di Bandung yang menyediakan hidangan khas Medan</p>
 <p class="text-left">
-Our work encompasses graphics and identity, products and packaging, exhibitions
-and installations, websites and digital experiences, advertising and
-communications, sound and motion.
+    <div class="img-box">
+        <img src="images/roda.jpeg" width="1000" height="720" alt="Tempat Dagang" >
+      </div> <br>
+Ketika kamu lapar, ya makan saja.
 </p>
 @endsection
--- resources/views/testimoni.blade.php
+++ resources/views/testimoni.blade.php
@@ -183,7 +183,8 @@
         role="button" data-slide="prev">
           <span class="sr-only">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#carouselExample2Controls" role="button" data-slide="next">
+        <a class="carousel-control-next" href="#carouselExample2Controls"
+        role="button" data-slide="next">
           <span class="sr-only">Next</span>
         </a>
       </div>
